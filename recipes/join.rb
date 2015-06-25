22120dddc3b9d99a67f96166dc8de118f06eab02
shaon <mdshaonimran@gmail.com>
2015-06-25 08:26:37 +0600

    fix idempotency while joining nodes

--- recipes/join.rb
+++ recipes/join.rb
@@ -1,5 +1,9 @@
 node_head = node['riakcs_cluster']['topology']['head']['fqdn']
 
-execute "join-cluster" do
-  command "riak-admin cluster join riak@#{node_head}"
+member_status = ""
+member_status = Mixlib::ShellOut.new("riak-admin member-status | grep #{node['hostname']}").run_command.stdout.strip
+if member_status == ""
+  execute "join-cluster" do
+    command "riak-admin cluster join riak@#{node_head}"
+  end
 end
